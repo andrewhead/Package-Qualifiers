45ec6878d18bb36eaa509e5122f5234d31c99d90
Andrew Head <head.andrewm@gmail.com>
2016-04-09 12:18:11 -0700

    Fetch: Script for getting snapshots of Stack Overflow questions

--- data.py
+++ data.py
@@ -18,7 +18,7 @@ data_logger.addHandler(log_handler)
 data_logger.propagate = False
 
 from models import create_tables, init_database
-from fetch import queries, results, results_content, histories
+from fetch import queries, results, results_content, histories, stack_overflow_questions
 from import_ import stackoverflow
 from compute import post_tags, tasks
 from migrate import run_migration
@@ -29,7 +29,7 @@ COMMANDS = {
     'fetch': {
         'description': "Fetch data from the web.",
         'module_help': "Type of data to fetch.",
-        'modules': [queries, results, results_content, histories],
+        'modules': [queries, results, results_content, histories, stack_overflow_questions],
     },
     'import': {
         'description': "Import data from a local data source.",
